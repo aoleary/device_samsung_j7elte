7b94b3807e6dfcf0f0a929a8cb00f2605254e405
aoleary <seanm187@gmail.com>
2022-01-20 10:29:21 +0000

    Xtend j7elte

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 LOCAL_PATH := device/samsung/j7elte
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_PATH)/lineage_j7elte.mk
+    $(LOCAL_PATH)/xtended_j7elte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_j7elte-user \
-    lineage_j7elte-userdebug \
-    lineage_j7elte-eng
+    xtended_j7elte-user \
+    xtended_j7elte-userdebug \
+    xtended_j7elte-eng
